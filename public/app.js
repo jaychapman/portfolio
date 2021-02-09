cfe4eeb68ef48126be9103eec13728adba4fafac
Jay C <jaychap77@yahoo.com>
2021-02-09 17:20:28 -0500

    update form

--- public/app.js
+++ public/app.js
@@ -1,8 +1,8 @@
 const hamburderBtn = document.getElementById('hamburgerBtn');
 const navBar = document.getElementById('navBar');
 const navLinks = document.getElementById('mobileNavLinkContainer');
-const closeBtn = document.getElementById('closeBtn');
 const form = document.getElementById('contactForm');
+const submitBtn = document.getElementById('submit');
 
 
 hamburderBtn.addEventListener('click', () => {
@@ -20,32 +20,42 @@ navLinks.addEventListener('click', () => {
 
 // Shows menu when user visits on mobile device
 function showNavInit(x) {
-    if (x.matches) { // If media query matches
-        navBar.classList.toggle('open');
-    }
+    if (x.matches)  navBar.classList.toggle('open');
   }
 
 const x = window.matchMedia("(max-width: 768px)")
 showNavInit(x) // Call listener function at run time
-x.addEventListener(showNavInit) // Attach listener function on state changes
+x.addEventListener(showNavInit, showNavInit) // Attach listener function on state changes
 
 
-const formEvent = form.addEventListener('submitBtn', (event) => {
+// const formEvent = form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     console.log('submit button clicked');
+//     let mail = new FormData(form);
+
+//     SendMail(mail);
+// });
+
+submitBtn.addEventListener('click', (event) => {
     event.preventDefault();
+    console.log("that worked!");
 
     let mail = new FormData(form);
+    sendMail(mail);
+})
 
-    Sendmail(mail);
-});
 
-const sendmail = (mail) => {
+const sendMail = (mail) => {
     fetch("https://stormy-fortress-18043.herokuapp.com/send", {
-        method: "post",
+        method: "POST",
         body: mail,
     }).then((response) => {
         return response.json();
     });
 };
+
+
+
 
 
 
