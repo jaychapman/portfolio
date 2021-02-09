b5d600f88a41e38272e702bba0e4897038522dbd
Jay C <jaychap77@yahoo.com>
2021-02-09 16:07:22 -0500

    testing contact form

--- public/app.js
+++ public/app.js
@@ -2,6 +2,8 @@ const hamburderBtn = document.getElementById('hamburgerBtn');
 const navBar = document.getElementById('navBar');
 const navLinks = document.getElementById('mobileNavLinkContainer');
 const closeBtn = document.getElementById('closeBtn');
+const form = document.getElementById('contactForm');
+
 
 hamburderBtn.addEventListener('click', () => {
     console.log('button clicked');
@@ -26,6 +28,24 @@ function showNavInit(x) {
 const x = window.matchMedia("(max-width: 768px)")
 showNavInit(x) // Call listener function at run time
 x.addEventListener(showNavInit) // Attach listener function on state changes
+
+
+const formEvent = form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let mail = new FormData(form);
+
+    Sendmail(mail);
+});
+
+const sendmail = (mail) => {
+    fetch("https://stormy-fortress-18043.herokuapp.com/send", {
+        method: "post",
+        body: mail,
+    }).then((response) => {
+        return response.json();
+    });
+};
 
 
 
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
-// add more dependencies
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+const multiparty = require('multiparty');
+require("dotenv").config();
+
 
 
 
@@ -8,14 +12,54 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 //cors
+app.use(cors({ origin: "* "}));
 
 app.use("/public", express.static(process.cwd() + "/public"));
 
 //transporter code
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS,
+    }
+});
 
 //verify connection
+transporter.verify(function (error, success) {
+    if (error) {
+        console.log(error);
+    }else {
+        console.log("server ready to take our messages")
+    }
+});
 
-//app.post
+app.post("/send", (req, res) => {
+    let form = new multiparty.Form();
+    let data = {};
+    form.parse(req, function (err, fields) {
+        console.log(fields);
+        Object.keys(fields).forEach(function (property) {
+            data[property] = fields[property].toString();
+        });
+        console.log(data);
+        const mail = {
+            sender: data.formName,
+            to: process.env.EMAIL,
+            subject: 'Contact Form Submission',
+            text: `${data.companyName} \n${data.formPhone} \n${data.formEmail} \n${data.message}`
+        };
+        transporter.sendMail(mail, (err, data) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send("something went wrong");
+            }else {
+                res.status(200).send("Email successfully sent to recipient");
+            }
+        });
+    });
+});
+
 
 app.route("/").get(function (req, res) {
     res.sendFile(process.cwd() + "/public/index.html");
